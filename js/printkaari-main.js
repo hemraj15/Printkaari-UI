d92d44c84b5bb392512e5205734bfa6d0b28bcc5
Hemraj Pamrar <hemraj.it12@gmail.com>
2017-02-02 04:36:36 +0530

    modification for single app multiple controllers app name chanegd

--- js/printkaari-main.js
+++ js/printkaari-main.js
@@ -1,4 +1,4 @@
-var app = angular.module('loginApp',[]);
+var app = angular.module('printkaariApp',[]);
 
 app.controller('loginController',['$scope', '$http', '$window',function($scope,$http,$window){
 	$scope.doLogin = function(){
@@ -111,4 +111,72 @@ app.controller('loginController',['$scope', '$http', '$window',function($scope,$
 			return str.join("&");    
     }
 
+}]);
+
+
+app.controller('locationController',['$scope', '$http', '$window',function($scope,$http,$window){
+
+$scope.getCountryList=function(){
+		
+		var onSuccess = function(response){
+			$scope.countryList=response.data;
+			console.log(response);
+		};
+		
+		var onError = function(error){
+			console.log(error);				
+		};
+		
+		$http.get('http://162.220.61.86:8080/printkaari-api/location/countries').then(onSuccess, onError);
+	}
+	
+		$scope.getStateListByCountryId=function(){
+		
+		var data = {
+			"countryId" : $scope.countryId
+			
+		}
+		console.log(data);
+		
+        var _config = {
+            headers: {
+            	'Content-Type' : 'application/json',
+			}
+        };
+		var onSuccess = function(response){
+			$scope.stateList=response.data;
+			console.log(response);
+		};
+		
+		var onError = function(error){
+			console.log(error);				
+		}
+		
+		$http.post('http://162.220.61.86:8080/printkaari-api/location/country/'+$scope.countryId+'/states' ,data,_config).then(onSuccess, onError);
+	}
+	
+	$scope.getCityListByStateId=function(){
+		
+		var data = {
+			"stateId" : $scope.stateId
+			
+		}
+		console.log(data);
+		
+        var _config = {
+            headers: {
+            	'Content-Type' : 'application/json',
+			}
+        };
+		var onSuccess = function(response){
+			$scope.cityList=response.data;
+			console.log(response);
+		};
+		
+		var onError = function(error){
+			console.log(error);				
+		};
+		
+		$http.post('http://162.220.61.86:8080/printkaari-api/location/states/'+$scope.stateId+'/cities' ,data,_config).then(onSuccess, onError);
+	}
 }]);
