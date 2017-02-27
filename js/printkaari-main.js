d8b348f3c698e53bd7c43fd7ea4b1c3a7964c45b
Kamlesh Uikey <kamlesh8111@gmail.com>
2017-02-27 21:33:09 +0530

    path made relative using directives

--- js/printkaari-main.js
+++ js/printkaari-main.js
@@ -2,5 +2,21 @@ var app = angular.module('printkaariApp',["ngRoute"]);
 
 
 app.controller('mainController',['$http', function($http){
-	
+	var main = this;
 }]);
+
+
+app.directive('pageHeader', function(){
+	return {
+		templateUrl: 'partials/header.html',
+		controller: 'navTabController',
+		controllerAs: 'navCtl'
+	}
+});
+
+
+app.directive('pageFooter', function(){
+	return {
+		templateUrl: 'partials/footer.html'
+	}
+});
