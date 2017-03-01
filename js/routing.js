ea4d530d853419e97f0ee35f3522c0963b2f744a
Kamlesh Uikey <kamlesh8111@gmail.com>
2017-03-01 09:02:41 +0530

    initial structure for dashboard section build

--- js/routing.js
+++ js/routing.js
@@ -6,6 +6,8 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 		.when('/emailtoken/:tokenId', {templateUrl: 'partials/login.html',   controller: 'loginController'})
 		.when('/reset-password/:tokenForPwd' ,{templateUrl: 'partials/login.html',   controller: 'loginController'})
 		.when('/custDashboard' ,{templateUrl: 'partials/custDashboard.html',   controller: 'dashboardController'})
+		.when('/myprofile' ,{templateUrl: 'partials/my-profile.html',   controller: 'myProfileController', controllerAs: 'myProfile'})
+		.when('/myorder' ,{templateUrl: 'partials/my-order.html',   controller: 'myOrderController', controllerAs: 'myOrder'})
 		.when('/admin' ,{templateUrl: 'partials/empDashboard.html',   controller: 'dashboardController'})
 		.when('/logout' ,{redirectTo: '/login.html'})
 		.otherwise({redirectTo: '/' });
