12430ae5f82434c97fb74f0bb6820c6fa41ecdf0
Kamlesh Uikey <kamlesh8111@gmail.com>
2017-03-08 19:21:23 +0530

    intermediate for checking

--- js/controller/cart-controller.js
+++ js/controller/cart-controller.js
@@ -1,5 +1,17 @@
 
 
-app.controller('cartController',[function(){
+app.controller('cartController',['cartService', function(cartService){
 	
+	var cart = this;
+
+
+	cart.init = function(){
+		cart.cartData = cartService.getCartData();
+	};
+
+	cart.checkout = function(){
+		
+	};
+
+	cart.init();
 }]);
--- js/controller/product-controller.js
+++ js/controller/product-controller.js
@@ -2,11 +2,18 @@
 
  */
 
-app.controller('productController', ['cartService', '$rootScope', function(cartService,$rootScope){
+app.controller('productController', ['cartService', 'orderFactory', '$rootScope', function(cartService,orderFactory,$rootScope){
 
 	var product = this;
 
+
 	product.addProduct = function(){
+		console.log(product.bindingType);
+		console.log(product.file);
+		console.log(product.noOfColoredPage);
+		console.log(product.noOfPage);
+		console.log(product.comment);
 		console.log("this is done");
 	};
+
 }]);
