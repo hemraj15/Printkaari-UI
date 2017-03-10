2a771c2ed7453d28a37923ed8e2c4c278b81e5cb
Kamlesh Uikey <kamlesh8111@gmail.com>
2017-03-10 09:29:03 +0530

    example set

--- js/controller/cart-controller.js
+++ js/controller/cart-controller.js
@@ -10,8 +10,37 @@ app.controller('cartController',['cartService', function(cartService){
 	};
 
 	cart.checkout = function(){
-		
+
 	};
+
+	cart.params = {
+		key: $MERCHANT_KEY,
+		hash: $hash,
+		txnid: $txnid,
+		amount: amount,
+		firstname: firstname
+		email: email,
+		phone: phone,
+		productinfo: productinfo,
+		surl: "http://printkaari.com/#!/payment/success",
+		furl: "http://printkaari.com/#!/payment/failure",
+		service_provider: "payu_paisa",
+		//Optional Parameter
+		lastname: lastname,
+		curl: "http://printkaari.com/#!/payment/cancel",
+		address1: "",
+		address2: "",
+		city: "",
+		state: "",
+		country: "",
+		zipcode: "",
+		udf1: "",
+		udf2: "",
+		udf3: "",
+		udf4: "",
+		udf5: "",
+		pg: ""
+	}
 
 	cart.init();
 }]);
